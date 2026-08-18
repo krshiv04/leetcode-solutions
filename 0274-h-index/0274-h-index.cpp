be0871c7ba24e1211100d40efb2f9d597c0f3046
Shivam kr <147191813+krshiv04@users.noreply.github.com>
2026-08-18 23:25:06 +0530

    Time: 0 ms (100%), Space: 12.3 MB (63.95%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int n=citations.size(), start=0,end=n-1, mid;
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            if(citations[mid]>=n-mid) end=mid-1;
+            else start=mid+1;
+        }
+        return n-start;
+
+    }
+};
