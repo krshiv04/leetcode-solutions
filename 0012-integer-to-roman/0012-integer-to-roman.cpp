ce3938f889ab918421643f6b73fcdf2859fad00b
Shivam kr <147191813+krshiv04@users.noreply.github.com>
2026-09-03 00:54:21 +0530

    Time: 4 ms (49.94%), Space: 9.3 MB (82.76%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,51 +1,11 @@
 class Solution {
 public:
-    string roman(int c)
-    {
-        if(c==1) return "I";
-        else if(c==2) return "II";
-        else if(c==3) return "III";
-        else if(c==4) return "IV";
-        else if(c==5) return "V";
-        else if(c==6) return "VI";
-        else if(c==7) return "VII";
-        else if(c==8) return "VIII";
-        else if(c==9) return "IX";
-        else if(c==10) return "X";
-        else if(c==20) return "XX";
-        else if(c==30) return "XXX";
-        else if(c==40) return "XL";
-        else if(c==50) return "L";
-        else if(c==60) return "LX";
-        else if(c==70) return "LXX";
-        else if(c==80) return "LXXX";
-        else if(c==90) return "XC";
-        else if(c==100) return "C";
-        else if(c==200) return "CC";
-        else if(c==300) return "CCC";
-        else if(c==400) return "CD";
-        else if(c==500) return "D";
-        else if(c==600) return "DC";
-        else if(c==700) return "DCC";
-        else if(c==800) return "DCCC";
-        else if(c==900) return "CM";
-        else if(c==1000) return "M";
-        else if(c==2000) return "MM";
-        else return "MMM";
-    }
     string intToRoman(int num) {
-        string ans;
-        int pow =1;
+        string thousands[4] = {"","M","MM","MMM"};
+        string hundereds[10] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string tens[10] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string ones[10] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
 
-        while(num > 0)
-        {
-            int temp = (num%10) * pow;
-            if(temp != 0)
-                ans = roman(temp) + ans;
-            num /= 10;
-            pow *= 10;
-        }
-
-        return ans;
+        return thousands[num/1000] + hundereds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
