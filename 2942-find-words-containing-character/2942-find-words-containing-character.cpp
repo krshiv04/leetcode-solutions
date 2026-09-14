e4cd15b54412a77cecd13d742ec2c6003b2a198b
Shivam kr <147191813+krshiv04@users.noreply.github.com>
2026-09-15 03:50:21 +0530

    Time: 0 ms (100%), Space: 33 MB (82.47%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.cpp
+++ 2942-find-words-containing-character/2942-find-words-containing-character.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
         vector<int> ans;
         for(int i=0; i<words.size(); i++)
-        if(words[i].contains(x)) ans.push_back(i);
+        if(words[i].find(x)!=string::npos) ans.push_back(i);
         return ans;
     }
 };
