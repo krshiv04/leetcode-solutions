4fb1d8b78b83fda0537b238e895fc44b5b64eb7a
Shivam kr <147191813+krshiv04@users.noreply.github.com>
2026-09-21 23:08:30 +0530

    Time: 0 ms (100%), Space: 13.9 MB (57.58%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.cpp
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int xorall=0, result1=0, result2=0;
+        int n=nums.size(), xorAll=0, result1=0, result2=0;
+        for(auto num:nums)
+        xorAll^=num;
 
-        for(int i=0; i<n; i++)
-        xorall^=nums[i];
+        int mask=xorAll&(-(unsigned int)xorAll);
 
-        int mask=xorall & (-(unsigned int)xorall);
-
-        for(int i=0; i<n; i++)
+        for(auto num:nums)
         {
-            if(nums[i]&mask)   result1^=nums[i];
-            else    result2^=nums[i];
+            if(num&mask) result1^=num;
+            else result2^=num;
         }
-        
 
-        vector<int>ans;
+        vector<int> ans;
         ans.push_back(result1);
         ans.push_back(result2);
-
         return ans;
 
     }
